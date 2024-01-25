205e2a76c95c21da0fcc093fa1b2e04926476064
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-01-25 09:56:18 +0530

    University practical's

---
+++ NumericUP_DOWN.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumericUpDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i, num;
+            num = Convert.ToInt16(numericUpDown1.Value);
+            listBox1.Items.Clear();
+            for(i=1;i<=10;i++)
+            {
+                listBox1.Items.Add(num + "*" + i + "=" + num * i);
+            }
+        }
+    }
+}
