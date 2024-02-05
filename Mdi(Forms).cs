e6bf639c651bde001ed5199a24b498ae217acbfe
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-02-05 10:27:03 +0530

    Unit 2 practical=>7

---
+++ Mdi(Forms).cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formsmdi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void form2ToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+            Form2 frm = new Form2();
+            frm.MdiParent = this;
+            frm.Show();
+
+        }
+
+        private void form3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Form3 frm = new Form3();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form frm = this.ActiveMdiChild;
+            frm.Close();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.openToolStripMenuItem.Checked = true;
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+
+        }
+
+        private void tileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void tileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void arrangeAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+    }
+}
